89fc43b8b6a1fa58899a036c7502e98283e0c795
MishaDemchyshyn <alex168959@gmail.com>
2020-05-10 15:57:24 +0300

    added the ability to add files

--- Plagiarism-Checker/Controllers/HomeController.cs
+++ Plagiarism-Checker/Controllers/HomeController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Plagiarism_Checker.Models;
 
@@ -26,6 +29,38 @@ namespace Plagiarism_Checker.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult ChooseFile()
+        {
+            return View();
+        }
+
+        private readonly IHostingEnvironment _appEnvironment;
+
+        [HttpPost]
+        public ActionResult AddFile(IFormFile[] files)
+        {
+            byte[] ByteArray = null;
+            for (int i = 0; i < files.Length; i++)
+            {
+                using (var binaryReader = new BinaryReader(files[i].OpenReadStream()))
+                {
+                    ByteArray = binaryReader.ReadBytes((int)files[i].Length);
+                }
+                using (UniverContext db = new UniverContext())
+                {
+
+
+
+                    db.Solution.Add(new Solution
+                    { File = ByteArray });
+                    db.SaveChanges();
+
+                }
+            }
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
