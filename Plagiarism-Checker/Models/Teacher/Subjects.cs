d2c1bd923ced10bb3626d28d8a7db74433df811c
publicgiraffe <zabavnoahah@gmail.com>
2020-05-16 00:33:18 +0300

    TaskAddition Ability

--- Plagiarism-Checker/Models/Teacher/Subjects.cs
+++ Plagiarism-Checker/Models/Teacher/Subjects.cs
@@ -7,15 +7,22 @@ namespace Plagiarism_Checker.Models.Teacher
 {
     public class Subjects
     {
-        public Subjects(string nameOfDiscipline, string group, string time)
+        public Subjects(string nameOfDiscipline, string group, string time,int groupId, int lessonId)
         {
             NameOfDiscipline = nameOfDiscipline;
             Group = group;
             Time = time;
+            GroupId = groupId;
+            LessonId = lessonId;
         }
 
         public string NameOfDiscipline { get; set; }
         public string Group { get; set; }
-        public string Time { get; set; }
+        public int GroupId { get; set; }
+        public int LessonId { get; set; }
+
+        public string Time
+        { get; set; }
 
     }
+}
--- Plagiarism-Checker/Models/Teacher/TaskAdd.cs
+++ Plagiarism-Checker/Models/Teacher/TaskAdd.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,29 @@ namespace Plagiarism_Checker.Models.Teacher
 {
     public class TaskAdd
     {
+
+        public _TaskAdd taskAdd;
+        public List<Subjects> subjects=new List<Subjects>();
+
+        public TaskAdd(List<Subjects> subjects)
+        {
+            this.subjects = subjects;
+        }
+    }
+    public class _TaskAdd
+    {
+        [Required(ErrorMessage = "Оберить тип завдання:")]
+        public bool IsTest { get; set; }
+        [Required(ErrorMessage = "Вкажіть термір виконання завдання:")]
+        [DataType(DataType.DateTime)]
+        public DateTime Deadline { get; set; }
+        [Required(ErrorMessage = "Заповніть вимоги:")]
+        public string Requirenments { get; set; }
+
+        [Required(ErrorMessage = "Заповніть поле з вибраною дисципліною тф группою:")]
+
+        public int Lesson { get; set; }
+
+  
     }
 }
